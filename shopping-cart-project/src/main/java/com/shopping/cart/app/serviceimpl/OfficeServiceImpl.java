8cfa65fc117b6f0a47e52882f8450565def43781
divyanshuTiwa <113118479+divyanshuTiwa@users.noreply.github.com>
2022-12-31 09:15:25 -0500

    Update OfficeServiceImpl.java

--- shopping-cart-project/src/main/java/com/shopping/cart/app/serviceimpl/OfficeServiceImpl.java
+++ shopping-cart-project/src/main/java/com/shopping/cart/app/serviceimpl/OfficeServiceImpl.java
@@ -9,5 +9,51 @@ import com.shopping.cart.app.service.OfficeService;
 public class OfficeServiceImpl implements OfficeService{
 	
 	private OfficeRepository officeRepository;
+	
+	@Override
+	public List<Office> findAll() {
+		return null;
+	}
+
+	@Override
+	public MessageResponse save(Office office) {
+		return null;
+	}
+
+	@Override
+	public Optional<Office> getById(long id) {
+		return Optional.empty();
+	}
+
+	@Override
+	public List<Office> sortedByState(Office officedetails) {
+		return null;
+	}
+
+	@Override
+	public List<Office> getByCity(String city) {
+		return null;
+	}
+
+	@Override
+	public List<Office> paging() {
+		return null;
+	}
+
+	@Override
+	public Iterable<Office> sorting(Office officedetails) {
+		return null;
+	}
+
+	@Override
+	public Office update(Office office) {
+		return null;
+	}
+
+	@Override
+	public MessageResponse deleteById(long id) {
+		return null;
+	}
+
 
 }
