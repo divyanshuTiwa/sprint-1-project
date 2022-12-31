1db223c0f3c9f5b1a22edd345c0f2f1612b9cde8
divyanshuTiwa <113118479+divyanshuTiwa@users.noreply.github.com>
2022-12-31 09:10:30 -0500

    Update ProductService.java

--- shopping-cart-project/src/main/java/com/shopping/cart/app/service/ProductService.java
+++ shopping-cart-project/src/main/java/com/shopping/cart/app/service/ProductService.java
@@ -1,5 +1,30 @@
 package com.shopping.cart.app.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.shopping.cart.app.model.Product;
+
+
 public interface ProductService {
+  
+  public List<Product> findAll();
+
+	public MessageResponse save(Product product);
+
+	public Optional<Product> getById(long id);
+
+	public List<Product> sortedByPrice(Product product);
+
+	public List<Product> getByName(String productName);
+
+	public List<Product> paging();
+
+	public Iterable<Product> sorting(Product product);
+
+	Product update(Product product) ;
+
 
 }
