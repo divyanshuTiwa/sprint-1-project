ec1c3adbc458fb67d5a96f792f85d417a7d68bb4
divyanshuTiwa <113118479+divyanshuTiwa@users.noreply.github.com>
2022-12-31 08:56:12 -0500

    Update OfficeService.java

--- shopping-cart-project/src/main/java/com/shopping/cart/app/service/OfficeService.java
+++ shopping-cart-project/src/main/java/com/shopping/cart/app/service/OfficeService.java
@@ -1,5 +1,30 @@
 package com.shopping.cart.app.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.shopping.cart.app.model.Office;
+
+
 public interface OfficeService {
+  
+  public List<Office> findAll();
+
+	public MessageResponse save(Office office);
+
+	public Optional<Office> getById(long id);
+
+	public List<Office> sortedByState(Office officedetails);
+
+	public List<Office> getByCity(String city);
+
+	public List<Office> paging();
+
+	public Iterable<Office> sorting(Office officedetails);
+
+	Office update(Office office) ;
+
 
 }
