872b0446e89307508cc2cc3593c2ea954a064e63
divyanshuTiwa <113118479+divyanshuTiwa@users.noreply.github.com>
2022-12-31 08:57:55 -0500

    Update OrderDetailService.java

--- shopping-cart-project/src/main/java/com/shopping/cart/app/service/OrderDetailService.java
+++ shopping-cart-project/src/main/java/com/shopping/cart/app/service/OrderDetailService.java
@@ -1,5 +1,28 @@
 package com.shopping.cart.app.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.shopping.cart.app.model.OrderDetail;
+
+
 public interface OrderDetailService {
+  
+  public List<OrderDetail> findAll();
+
+	public MessageResponse save(OrderDetail orderDetails);
+
+	public Optional<OrderDetail> getById(long id);
+
+	public List<OrderDetail> findGreaterthanEqualByPrice(double priceEach);
+
+	public List<OrderDetail> paging();
+
+	public Iterable<OrderDetail> sorting(OrderDetail orderDetails);
+
+	OrderDetail update(OrderDetail orderDetails) ;
+
 
 }
