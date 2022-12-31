79f3d1671253795dbe5c6f4a6398c8f3471b532f
divyanshuTiwa <113118479+divyanshuTiwa@users.noreply.github.com>
2022-12-31 09:05:32 -0500

    Update OrderService.java

--- shopping-cart-project/src/main/java/com/shopping/cart/app/service/OrderService.java
+++ shopping-cart-project/src/main/java/com/shopping/cart/app/service/OrderService.java
@@ -1,5 +1,27 @@
 package com.shopping.cart.app.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.shopping.cart.app.model.Order;
+
 public interface OrderService{
+  
+  public List<Order> findAll();
+
+	public MessageResponse save(Order order);
+
+	public Optional<Order> getById(long id);
+
+	public List<Order> getByStatus( boolean status);
+
+	public List<Order> paging();
+
+	public Iterable<Order> sorting(Order order);
+
+	Order update(Order order) ;
+
 
 }
