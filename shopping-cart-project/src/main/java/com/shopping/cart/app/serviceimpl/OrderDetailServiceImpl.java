7f6c4852ade07e7747cca0d38417a444704f806f
divyanshuTiwa <113118479+divyanshuTiwa@users.noreply.github.com>
2022-12-31 09:16:29 -0500

    Update OrderDetailServiceImpl.java

--- shopping-cart-project/src/main/java/com/shopping/cart/app/serviceimpl/OrderDetailServiceImpl.java
+++ shopping-cart-project/src/main/java/com/shopping/cart/app/serviceimpl/OrderDetailServiceImpl.java
@@ -11,5 +11,46 @@ public class OrderDetailServiceImpl implements OrderDetailService {
 	
 	@Autowired
 	private OrderDetailRepository orderDetailRepository;
+	
+	@Override
+	public List<OrderDetail> findAll() {
+		return null;
+	}
+
+	@Override
+	public MessageResponse save(OrderDetail orderDetails) {
+		return null;
+	}
+
+	@Override
+	public Optional<OrderDetail> getById(long id) {
+		return Optional.empty();
+	}
+
+	@Override
+	public List<OrderDetail> findGreaterthanEqualByPrice(double priceEach) {
+		return null;
+	}
+
+	@Override
+	public List<OrderDetail> paging() {
+		return null;
+	}
+
+	@Override
+	public Iterable<OrderDetail> sorting(OrderDetail orderDetails) {
+		return null;
+	}
+
+	@Override
+	public OrderDetail update(OrderDetail orderDetails) {
+		return null;
+	}
+
+	@Override
+	public MessageResponse deleteById(long id) {
+		return null;
+	}
+
 
 }
