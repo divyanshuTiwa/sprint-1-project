4875b4d77c9d294d8e520709213252de962c7d34
divyanshuTiwa <113118479+divyanshuTiwa@users.noreply.github.com>
2022-12-31 09:12:31 -0500

    Update CustomerServiceImpl.java

--- shopping-cart-project/src/main/java/com/shopping/cart/app/serviceimpl/CustomerServiceImpl.java
+++ shopping-cart-project/src/main/java/com/shopping/cart/app/serviceimpl/CustomerServiceImpl.java
@@ -7,6 +7,76 @@ public class CustomerServiceImpl implements CustomerService {
 	
 	private CustomerRepository customerRepository;
 	
-	
+	@Override
+	public Optional<Customer> findById(long id) {
+		return Optional.empty();
+	}
+
+
+	@Override
+	public Customer save(Customer entity) {
+		return null;
+	}
+
+
+	@Override
+	public void delete(long id) {
+		
+	}
+
+
+	@Override
+	public List<Customer> findAll() {
+		return null;
+	}
+
+
+	@Override
+	public List<Customer> findByContactFirstNameAndContactLastName(String firstName, String lastName) {
+		return null;
+	}
+
+
+	@Override
+	public List<Customer> findByCityAndState(String city, String state) {
+		return null;
+	}
+
+
+	@Override
+	public List<Customer> findByPhoneNumber(String phone) {
+		return null;
+	}
+
+
+	@Override
+	public List<Customer> findAllCustomerOrderByCreditLimit() {
+		return null;
+	}
+
+
+	@Override
+	public List<Customer> findByCreditLimitLessThan(double creditLimit) {
+		return null;
+	}
+
+
+	@Override
+	public List<Customer> findByCreditLimitGreaterThan(double creditLimit) {
+		return null;
+	}
+
+
+	@Override
+	public List<Customer> paging() {
+		return null;
+	}
+
+
+	@Override
+	public Iterable<Customer> sorting(Customer customerdetails) {
+		return null;
+	}
+
 
 }
