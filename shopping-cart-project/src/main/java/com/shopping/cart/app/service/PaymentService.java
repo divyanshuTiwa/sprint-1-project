0b73cb0596e3a770caea9addbded5ebf4db7913c
divyanshuTiwa <113118479+divyanshuTiwa@users.noreply.github.com>
2022-12-31 09:07:21 -0500

    Update PaymentService.java

--- shopping-cart-project/src/main/java/com/shopping/cart/app/service/PaymentService.java
+++ shopping-cart-project/src/main/java/com/shopping/cart/app/service/PaymentService.java
@@ -1,5 +1,28 @@
 package com.shopping.cart.app.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.shopping.cart.app.model.Payment;
+
+
 public interface PaymentService {
+  
+  public List<Payment> findAll();
+
+	public MessageResponse save(Payment payment);
+
+	public Optional<Payment> getById(long id);
+
+	public List<Payment> sortedByAmount(Payment payment);
+
+	public List<Payment> paging();
+
+	public Iterable<Payment> sorting(Payment payment);
+
+	Payment update(Payment payment) ;
+
 
 }
