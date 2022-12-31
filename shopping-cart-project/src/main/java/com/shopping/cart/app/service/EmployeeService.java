980d8b668247770419a0f9050418e26c58290422
divyanshuTiwa <113118479+divyanshuTiwa@users.noreply.github.com>
2022-12-31 08:54:26 -0500

    Update EmployeeService.java

--- shopping-cart-project/src/main/java/com/shopping/cart/app/service/EmployeeService.java
+++ shopping-cart-project/src/main/java/com/shopping/cart/app/service/EmployeeService.java
@@ -1,5 +1,30 @@
 package com.shopping.cart.app.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.shopping.cart.app.model.Employee;
+
+
 public interface EmployeeService {
+  
+  public List<Employee> findAll();
+
+	public MessageResponse save(Employee employee);
+
+	public Optional<Employee> getById(long id);
+
+	public List<Employee> sortedByEmail(Employee employeedetails);
+
+	public List<Employee> getByName(String firstName);
+
+	public List<Employee> paging();
+
+	public Iterable<Employee> sorting(Employee employeedetails);
+
+	Employee update(Employee employee) ;
+
 
 }
