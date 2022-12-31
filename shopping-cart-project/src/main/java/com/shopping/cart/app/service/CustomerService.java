6a1370127eccc098270ca18034157ae546628e46
divyanshuTiwa <113118479+divyanshuTiwa@users.noreply.github.com>
2022-12-31 08:51:01 -0500

    Update CustomerService.java

--- shopping-cart-project/src/main/java/com/shopping/cart/app/service/CustomerService.java
+++ shopping-cart-project/src/main/java/com/shopping/cart/app/service/CustomerService.java
@@ -1,5 +1,30 @@
 package com.shopping.cart.app.service;
 
 public interface CustomerService{
+  
+  Optional<Customer> findById(long id);
+	
+	Customer save(Customer entity);
+	
+	void delete (long id);
+	 
+	List <Customer> findAll();
+	
+	List <Customer> findByContactFirstNameAndContactLastName(String firstName, String lastName);
+	
+	List <Customer> findByCityAndState(String city, String state);
+	
+	public List<Customer> paging();
+
+	public Iterable<Customer> sorting(Customer customerdetails);
+	
+	List <Customer> findByPhoneNumber(String phone);
+	
+	List <Customer> findAllCustomerOrderByCreditLimit();
+	
+	List<Customer>findByCreditLimitLessThan(double creditLimit);
+	
+	List<Customer> findByCreditLimitGreaterThan(double creditLimit);
+	
 
 }
